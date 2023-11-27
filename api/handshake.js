8b4d5b75952fefbad442455f306688870ca6218f
arielherself <ariel@arielherself.xyz>
2023-11-27 13:01:46 +0800

    chore: bump version

--- api/handshake.js
+++ api/handshake.js
@@ -1,9 +1,11 @@
 const __DEBUG__=1,
       __APP_VERSION__='v0.1.2a',
       __APP_INTRO__=`
-<b>Relocating Support</b><br>
-Can't find where you are from? We now memorize the last location you visited. Tap the "Relocate" button to find your way home.<br><br>
-<i>This update also include several stability and UI improvements.</i>
+<b>Relocating Support</b>
+Can't find where you are from? We now memorize the last location you visited. Tap the "Relocate" button to find your way home.<br>
+<b>Nearby Search</b>
+You can first locate a general area and schedule detailed travel plan later. Tap the magnifier/gear icon to toggle between Nearby Search mode and Global Search mode.
+<i>This update also includes several stability and UI improvements.</i>
 `;
 
 export default function handler(req,res){
