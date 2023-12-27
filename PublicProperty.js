6c4923bf54fb3ee4a447c1dfea4894a9a5a01773
arielherself <ariel@arielherself.xyz>
2023-12-27 17:17:08 +0800

    chore: bump version

--- PublicProperty.js
+++ PublicProperty.js
@@ -1,9 +1,9 @@
 // no dependencies
 
 export const __DEBUG__ = 1;
-export const __APP_VERSION__ = 'v0.1.4a';
+export const __APP_VERSION__ = 'v0.2.0a';
 
 export const __APP_INTRO__ = `
-<b>Right way to follow.</b><br>
-In this update you can find a shortest route for any given start and destination.<br>
+<b>Algorithm improvement.</b><br>
+We are introducing a new pre-processing method called Obvious in this version.<br>
 `;
