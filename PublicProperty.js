9eb467077e317eb3c16a712f89145ec5d748a5c1
arielherself <ariel@arielherself.xyz>
2023-12-27 14:37:38 +0800

    chore: bump version

--- PublicProperty.js
+++ PublicProperty.js
@@ -1,7 +1,7 @@
 // no dependencies
 
 export const __DEBUG__ = 1;
-export const __APP_VERSION__ = 'v0.1.3a';
+export const __APP_VERSION__ = 'v0.1.4a';
 
 export const __APP_INTRO__ = `
 <b>Right way to follow.</b><br>
