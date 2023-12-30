aae6df3269f1d24171e7443ceaf87bdb73724735
arielherself <ariel@arielherself.xyz>
2023-12-30 11:15:22 +0800

    chore: bump version

--- PublicProperty.js
+++ PublicProperty.js
@@ -1,11 +1,13 @@
 // no dependencies
 
-export const __DEBUG__ = 1;
-export const __APP_VERSION__ = 'v0.3.0a';
+export const __DEBUG__ = 0;
+export const __APP_VERSION__ = 'v0.3.1-beta';
 
 export const __APP_INTRO__ = `
 <b>Algorithm improvement.</b><br>
 Our new routing solution is based on the Obvious A-Star algorithm now, with 2~10x faster speed in calculations.<br>
 <b>Support concatenating paths.</b><br>
 You can add points sequentially on the map.<br>
+<b>Clear the map.</b><br>
+Clear all markers with one click.<br>
 `;
